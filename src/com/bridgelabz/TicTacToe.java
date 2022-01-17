0a4ea045a38fa87e087ee905680cafb144cb415d
Amol Ghotale <amol9.ag@gmail.com>
2022-01-17 23:57:35 +0530

    ADDED: Game board

---
+++ src/com/bridgelabz/TicTacToe.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class TicTacToe {
+
+    public static void TicTacToeInit() {
+        int i = 0;
+        char[] ticTacToe = new char[10];
+        for (i = 0; i < 10; i++) {
+            ticTacToe[i] = ' ';
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
