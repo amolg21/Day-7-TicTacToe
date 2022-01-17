979986a851f87900a9e3ad46ded3fb4257136fa2
Amol Ghotale <amol9.ag@gmail.com>
2022-01-18 00:39:24 +0530

    ADDED and MODIFIED: UC-2_&_UC-3

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -4,22 +4,49 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+    static int i;
+    static char[] ticTacToe = new char[10];
+    static char letter;
+
     public static void TicTacToeInit() {
-        int i = 0;
-        char[] ticTacToe = new char[10];
-        for (i = 0; i < 10; i++) {
+        for (i = 1; i < 10; i++) {
             ticTacToe[i] = ' ';
         }
     }
-      public static void ChooseValue() {
 
-          Scanner sc = new Scanner(System.in);
-          System.out.println("Enter the letter X or O");
-          char ch = sc.next().charAt(0);
+    public static void ChooseValue() {
+
+        while (letter != 'X' && letter != 'O') {
+            System.out.println("Please Select the Letter: X or O");
+
+            //Scanner Method for User Input
+            Scanner sc = new Scanner(System.in);
+            letter = sc.next().charAt(0);
+            switch (letter) {
+                case 'X':
+                    System.out.println("Player Selected " + letter + " Symbol");
+                    break;
+                case 'O':
+                    System.out.println("Player Selected " + letter + " Symbol");
+                    break;
+                default:
+                    System.out.println("Please Select the Symbol From X or O only");
+                    break;
+
+            }
+        }
+    }
+
+    static void ShowBoard() {
+        for (i = 1; i < 10; i++) {
+            System.out.println(ticTacToe[i] + " | " + ticTacToe[i++] + " | " + ticTacToe[i++] + '\n' + " --- ");
 
         }
+    }
 
     public static void main(String[] args) {
-        // write your code here
+        TicTacToeInit();
+        ChooseValue();
+        ShowBoard();
     }
 }
