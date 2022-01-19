d14c171d18300da96715ea60ad6b91f3a58fe170
Amol Ghotale <amol9.ag@gmail.com>
2022-01-19 13:09:37 +0530

    ADDED and MODIFIED: Making move

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -7,46 +7,46 @@ public class TicTacToe {
     static int i;
     static char[] ticTacToe = new char[10];
     static char letter;
+    static Scanner sc = new Scanner(System.in);
 
     public static void TicTacToeInit() {
-        for (i = 1; i < 10; i++) {
+        for (i = 0; i < 10; i++) {
             ticTacToe[i] = ' ';
         }
     }
 
     public static void ChooseValue() {
 
-        while (letter != 'X' && letter != 'O') {
-            System.out.println("Please Select the Letter: X or O");
+        System.out.println("Please Select the Letter: X or O");
 
-            //Scanner Method for User Input
-            Scanner sc = new Scanner(System.in);
-            letter = sc.next().charAt(0);
-            switch (letter) {
-                case 'X':
-                    System.out.println("Player Selected " + letter + " Symbol");
-                    break;
-                case 'O':
-                    System.out.println("Player Selected " + letter + " Symbol");
-                    break;
-                default:
-                    System.out.println("Please Select the Symbol From X or O only");
-                    break;
+        //Scanner Method for User Input
+        letter = sc.next().charAt(0);
+        // System.out.println("Enter the index you want to enter the value (1-9)");
+        // int index = sc.nextInt();
+        // ticTacToe[index - 1] = letter;
 
-            }
-        }
     }
 
     static void ShowBoard() {
-        for (i = 1; i < 10; i++) {
-            System.out.println(ticTacToe[i] + " | " + ticTacToe[i++] + " | " + ticTacToe[i++] + '\n' + " --- ");
+        for (i = 0; i <= 8; i++) {
+            System.out.println(ticTacToe[i] + " | " + ticTacToe[++i] + " | " + ticTacToe[++i] + '\n' + "---------");
+        }
+    }
 
+    public static void MakeMove() {
+        System.out.println("Enter the index you want to enter the value (1-9)");
+
+        int index = sc.nextInt();
+        if (ticTacToe[index-1] == ' ') {
+            ticTacToe[index - 1] = letter;
         }
     }
 
     public static void main(String[] args) {
         TicTacToeInit();
         ChooseValue();
+        ShowBoard();
+        MakeMove();
         ShowBoard();
     }
 }
